e111efd634d7c159c8f6ed915b049e19e5c07d7a
PapiCLY <christianyehudah@gmail.com>
2024-11-05 00:43:18 -0500

    5-day weather complete

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const now =  new Date();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', (req,res)=>{
     res.render('index.ejs')
